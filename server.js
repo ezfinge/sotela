425e7bd76d03917932f95ecc28207355615d7b2f
ezfinge <73872184+ezfinge@users.noreply.github.com>
2026-09-07 02:01:57 -0300

    Update server.js
    ajuste na porta
    e server listen linha 166

--- server.js
+++ server.js
@@ -158,7 +158,11 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
